dda5a1325013cab39d4d5a9d9ed77242d7d8688f
Justin To <tojustn@gmail.com>
2024-09-15 16:25:14 -0500

    Login Styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import Form from "../components/Form"
 import "../styles/Login.css"
+import manstudying from '../assets/static/images/manstudying.jpg';
+import manstudyingcoffee from '../assets/static/images/manstudyingcoffee.png'
 
 import { useNavigate } from "react-router-dom"
 function Login(){
@@ -9,13 +11,22 @@ function Login(){
         navigate("/register");
     }
     return (
-    <>
+    <div className = 'login-container'>
+    <div className = "welcome-text">
+        <h1>Ready to work more efficiently?</h1>
+        <br/>
+        <h2> Log In to Continue</h2>
+    </div>
+    <img className = "study-image" src={manstudying}></img>
+    <img className = "study-image-coffee" src={manstudyingcoffee}></img>
+    <div className = 'login-form'>
     <Form route = "api/token/" method = "login"/>
+    </div>
     <div className = "register-link">
         <p className = 'no-account'>Don't have an account?</p>
         <button onClick = {handleClick} className = 'register-link-button'> Register here </button>
     </div>
-    </>
+    </div>
     );
 }
 
