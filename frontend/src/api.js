1343493ba6487d16aff27ba188a1d44c1235b9a2
Justin To <tojustn@gmail.com>
2024-09-15 18:04:44 -0500

    Retry Sevice URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/focus-timer/backend/v1https://78548a6b-4ed3-4199-8569-05f0c0cf4c82-dev.e1-us-cdp-2.choreoapis.dev/focus-timer/backend/v1";
+const apiUrl = "	/choreo-apis/focus-timer/backend/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
