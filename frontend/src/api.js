9b8680d328d4f4129c9af42972191470ba90039a
Justin To <tojustn@gmail.com>
2024-09-15 18:13:57 -0500

    AGAIN

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,9 @@ import { ACCESS_TOKEN } from "./constants";
 const apiUrl = "/choreo-apis/focus-timer/backend/v1";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: import.meta.env.VITE_API_URL 
+    ? `${import.meta.env.VITE_API_URL}${apiUrl}`
+    : apiUrl,
 });
 
 api.interceptors.request.use(
